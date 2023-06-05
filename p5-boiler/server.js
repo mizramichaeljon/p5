c4e47fdd6cd76ab5eebad253831804a48c38367c
Michael-Jon Mizra <61235325+mizramichaeljon@users.noreply.github.com>
2023-06-05 15:56:43 +0100

    background-human-error

--- p5-boiler/server.js
+++ p5-boiler/server.js
@@ -3,10 +3,14 @@ const app = express();
 const p5port = 3000;
 
 
-// Serve the "draw-website" and "pin-website" directories
+//-- Serve the directory -- //
+/* 
+the first argument is the web-address we attach to the end of the localhost:{port}/p5
+the second argument is where to find the folder with the data
+ */
 app.use('/p5', express.static(__dirname + '/public'));
 
 // Start the server
 app.listen(p5port, () => {
-  console.log(`Draw-Website server running on port ${p5port}`);
+  console.log(`the server running on port ${p5port}`);
 });
