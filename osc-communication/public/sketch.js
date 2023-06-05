bfb431d06c181efd09c016fffda0729fee048688
Michael-Jon Mizra <61235325+mizramichaeljon@users.noreply.github.com>
2023-06-05 17:20:17 +0100

    modified:   .DS_Store 	new file:   osc-communication/.DS_Store 	renamed:    osc-communication/public/osc.js -> osc-communication/osc.js 	modified:   osc-communication/package-lock.json 	modified:   osc-communication/package.json 	new file:   osc-communication/public/.DS_Store 	modified:   osc-communication/public/index.html 	new file:   osc-communication/public/osc-browser.min.js 	new file:   osc-communication/public/second-page.html 	modified:   osc-communication/public/sketch.js 	modified:   osc-communication/public/style.css 	modified:   osc-communication/server.js

--- osc-communication/public/sketch.js
+++ osc-communication/public/sketch.js
@@ -1,7 +1,25 @@
+let socket;
+
 function setup() {
-    createCanvas(400, 400);
+    let canvas = createCanvas(400, 400);
+    canvas.parent('canvas');
+    // Set up a WebSocket connection to our server
+    socket = new WebSocket('ws://localhost:8000');
+
 }
 
 function draw() {
-    background(255,64,64);
+    background(255, 64, 64);
+}
+
+
+function mousePressed() {
+    // Define an OSC message
+    const oscMessage = new osc.default.Message('/test', mouseX, mouseY);
+
+    // Convert OSC message to a Uint8Array (binary data)
+    const binaryOscMessage = osc.default.writePacket(oscMessage);
+
+    // Send OSC message over WebSocket
+    socket.send(binaryOscMessage.buffer, { binary: true });
 }
--- osc-communication/server.js
+++ osc-communication/server.js
@@ -1,22 +1,50 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const osc = require('osc-js');
 const app = express();
 const port = 3000; // Choose a port number of your choice
 
 // Define a route to serve your website files
 
 //osc-communication will be the web-address 
-app.use('/osc-communication', express.static(__dirname + '/public'));
+app.use('/osc-communication',express.static('public'));
+
+
 // Define routes for specific HTML pages
-app.get('/osc-communication/index', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-
-app.get('/osc-communication/contact', (req, res) => {
-  res.sendFile(__dirname + '/public/contact.html');
+app.get('/osc-communication/second-page', (req, res) => {
+  res.sendFile(__dirname + '/public/second-page.html');
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Listen on port 8000
+server.listen(8000, () => {
+  console.log('Server listening on port 8000');
+});
+
+// Create a WebSocket server
+const wss = new WebSocket.Server({
+  server: server
+});
+
+wss.on('connection', (ws) => {
+  const oscPlugin = new osc.WebsocketServerPlugin();
+
+  oscPlugin.open();
+
+  ws.on('message', (msg) => {
+    const oscMsg = oscPlugin.parseBuffer(new Uint8Array(msg));
+    oscPlugin.send(oscMsg, { host: 'localhost', port: 57121 });
+  });
+
+  ws.on('close', () => {
+    oscPlugin.close();
+  });
 });
